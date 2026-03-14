7cb439bfbc2a6507dfa1da6952062319a8046a7c
marmishaw14 <marmishaw@academic.rrc.ca>
2026-03-13 23:27:29 -0500

    feat(sidequests): add hackathons/sidequests page

---
+++ app/sidequests/page.tsx
@@ -0,0 +1,11 @@
+import Navigation from '@/components/Navigation'
+import Sidequests from '@/components/Sidequests'
+
+export default function SidequestsPage() {
+  return (
+    <main className="min-h-screen bg-[#000000]">
+      <Navigation />
+      <Sidequests />
+    </main>
+  )
+}
