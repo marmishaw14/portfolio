fc9838e8cde03dc6712583da94da211df4a0d1b3
marmishaw14 <marmishaw@academic.rrc.ca>
2026-03-14 00:00:52 -0500

    feat: initial gallery of sidequests and hackathons

--- components/Sidequests.tsx
+++ components/Sidequests.tsx
@@ -4,33 +4,51 @@ type Sidequest = {
   title: string
   location: string
   image: string
+  status: string
 }
 
 const sidequests: Sidequest[] = [
   {
-    title: 'Hack 404',
-    location: 'Toronto, ON',
-    image: '/hackathons-sidequests/hack404.jpeg',
+    title: 'Sprint to Innovate 2025',
+    location: 'Winnipeg, MB',
+    image: '/hackathons-sidequests/sprint-to-innovate-2025.jpg',
+    status: '1st Place ($5,000)',
   },
   {
-    title: 'Hack the 6ix 2025',
-    location: 'Toronto, ON',
-    image: '/fr3sh_pfp.jpg',
+    title: 'RampUp Weekend 2025',
+    location: 'Winnipeg, MB',
+    image: '/hackathons-sidequests/rampup2025.jpg',
+    status: 'Finalists',
   },
   {
     title: 'SpurHacks 2025',
     location: 'Waterloo, ON',
-    image: '/fr3sh_pfp.jpg',
+    image: '/hackathons-sidequests/spurhacks2025.jpg',
+    status: 'Best Web3 and Best Quantum Startup ($7,000)',
+  },
+  {
+    title: 'Hack 404 2025',
+    location: 'Toronto, ON',
+    image: '/hackathons-sidequests/hack404.jpeg',
+    status: 'Participated',
+  },
+  {
+    title: 'Hack the 6ix 2025',
+    location: 'Toronto, ON',
+    image: '/hackathons-sidequests/hackthe6ix.jpeg',
+    status: 'Participated',
   },
   {
     title: 'Winnipeg Open Data Datathon 2026',
     location: 'Winnipeg, MB',
-    image: '/fr3sh_pfp.jpg',
+    image: '/hackathons-sidequests/datathon2026.jpeg',
+    status: '1st Place',
   },
   {
     title: 'Southern Manitoba Tech Conference Hackathon 2026',
     location: 'Winnipeg, MB',
-    image: '/fr3sh_pfp.jpg',
+    image: '/hackathons-sidequests/southernmbtechconf.png',
+    status: 'Participated',
   },
 ]
 
@@ -51,7 +69,12 @@ export default function Sidequests() {
               <div className="flex justify-center">
                 <PixelImage src={item.image} grid="6x4" />
               </div>
-              <h3 className="text-center text-xl font-semibold text-white">{item.title}</h3>
+              <div className="text-center">
+                <h3 className="text-xl font-semibold text-white">{item.title}</h3>
+                <p className="mt-1 text-sm text-gray-400">
+                  {item.location} | {item.status}
+                </p>
+              </div>
             </div>
           ))}
         </div>
