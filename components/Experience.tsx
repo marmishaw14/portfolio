5331e7cad04dbe4eb9ecdfa2967fa7b85106f9b8
McCauley Armishaw <151598523+marmishaw14@users.noreply.github.com>
2026-04-16 20:30:20 -0500

    chore(Experience): update duration header for MBLL role

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -14,9 +14,9 @@ const experiences: Experience[] = [
     title: 'IT Business Analyst Summer Student',
     organization: 'Manitoba Liquor and Lotteries Corporation',
     description: 'Incoming Summer 2026 on the Architecture and Analysis team.',
-    duration: 'May 2026 - Present',
+    duration: 'Incoming May 2026',
     location: 'Winnipeg, MB',
-    logo: '/logos/althra_logo.jpeg',
+    logo: '/logos/mbll.jpg',
   },
   {
     title: 'Venture Scout',
